eb410ed45e083084545266380c7514dc2bd6f525
Carlos M <alberto.meza.s@gmail.com>
2023-10-18 11:27:00 -0600

    Update files.py
    pylint and code styling

--- utilities/files.py
+++ utilities/files.py
@@ -4,12 +4,12 @@ from settings import config
 
 
 def get_files_by_size(path):
+    """Create a list of files in directory along with the size"""
     images_files = []
 
     fun = lambda x: os.path.isfile(os.path.join(path, x))
     files_list = filter(fun, os.listdir(path))
 
-    """Create a list of files in directory along with the size"""
     size_of_file = [(f, os.stat(os.path.join(path, f)).st_size) for f in files_list]
 
     for file in size_of_file:
@@ -20,10 +20,12 @@ def get_files_by_size(path):
 
 
 def get_name_girl(image):
+    """Get name by filename"""
     return image[: image.find("_")]
 
 
 def delete_images_tweeted(images_to_delete):
+    """Delete files in array object"""
     for image in images_to_delete:
         image_url = f"${config.ROUTE}/${image}"
         os.remove(image_url)
